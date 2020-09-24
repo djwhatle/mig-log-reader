96c3320f0ca627d909f624ee88c1eb02feeaef2e
Derek Whatley <derekwhatley@gmail.com>
2020-09-23 20:39:16 -0400

    Ignore known startup noise for mig registry

--- stern.sh
+++ stern.sh
@@ -6,5 +6,6 @@ stern "migration-controller|migration-ui|registry|restic|velero" \
 --exclude "watch is too old"  \
 --exclude "level=debug msg=.*s3aws.Stat" \
 --exclude "Found new dockercfg secret" \
+--exclude "Ignoring unrecognized environment variable REGISTRY" \
 --since 5s \
 --kubeconfig ${CONFIGPATH} \
